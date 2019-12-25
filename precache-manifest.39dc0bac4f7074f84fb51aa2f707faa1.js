46577ddaf73ddf2111e6f8a890ff9954288fb870
Alina <alina90565@gmail.com>
2019-12-25 12:53:25 +0200

    Updates

---
+++ precache-manifest.39dc0bac4f7074f84fb51aa2f707faa1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d461e08ca44347ac20e150cb45b22632",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "14faf49e29fe90b35d98",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.e37dc655.chunk.js"
+  },
+  {
+    "revision": "d5c79e279684a79c9a37",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.1b1a0a3e.chunk.js"
+  },
+  {
+    "revision": "252708e8cc807a96dcc4",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.324e605c.js"
+  }
+]);
